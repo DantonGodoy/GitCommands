a036fa7ef4e99310dd844e993b942fe59d7147cf
Danton de Godoy Petri Lima <dantongplima@gmail.com>
2018-10-06 23:46:39 -0300

    Updating project

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # GitCommands
 *Still a work in progress*
+
+# Starting the project:
+Run the following command to start the application: gulp
--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,44 @@
 const gulp = require('gulp')
   , sass = require('gulp-sass')
+  , clean = require('gulp-clean')
   , browserSync = require('browser-sync').create();
 
-// Complies Sass files to Css files
-gulp.task('scss-compile', function(){
+// Clean dist directory
+gulp.task('clean', function () {
+    gulp.src('dist')
+        .pipe(clean());
+});
+
+// Compile Sass to CSS files
+gulp.task('scss-compile', () => {
     gulp.src('project/scss/**/*')
         .pipe(sass())
-        .pipe(gulp.dest('project/css'))
+        .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.reload({stream: true}));
 });
 
+// Import the original HTML file to dist directory
+gulp.task('html-copy', () => {
+    gulp.src('project/index.html')
+        .pipe(gulp.dest('dist'));
+});
+
+// Import the original .js file to dist directory
+gulp.task('js-copy', () => {
+    gulp.src('project/js/**/*')
+        .pipe(gulp.dest('dist/js'));
+});
+
 // Start BrowserSync
-gulp.task('serve', function(){
+gulp.task('serve', () => {
     browserSync.init({
         server: {
-            baseDir: 'project/'
+            baseDir: 'dist/'
         }
     });
 
-    gulp.watch('project/scss/**/*', ['scss-compile']);
-    gulp.watch('project/**/*.html').on('change', browserSync.reload);
+    gulp.watch('dist/scss/**/*', ['scss-compile']);
+    gulp.watch('dist/**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('default', ['scss-compile', 'serve']);
+gulp.task('default', ['clean', 'html-copy', 'js-copy', 'scss-compile', 'serve']);
